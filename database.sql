1d47abfcdab60d44eccc4411a3539669e7731ef2
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 13:25:24 -0500

    added question table and updated Quizzes table

--- database.sql
+++ database.sql
@@ -6,3 +6,20 @@ CREATE TABLE Users(
   email EMAIL NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL
 )
+
+CREATE TABLE Quizzes(
+  quiz_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES Users(user_id),
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(255) NOT NULL,
+  is_admin_created BOOLEAN NOT NULL DEFAULT true,
+  create_at TIMESTAMP DEFAULT NOW(),
+  update_at TIMESTAMP NOT NULL NOW()
+)
+
+CREATE TABLE Questions(
+  question_id SERIAL PRIMARY KEY,
+  quiz_id INT NOT NULL REFERENCES Quizzes(quiz_id),
+
+)
