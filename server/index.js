86e44e61efa2dd38e1e2fbba6d91998a2bcd83bc
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 12:21:43 -0500

    allowing middleware to use json

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require('cors');
 
 //Middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(8000, () => {
   console.log('server has started on port 8000');
