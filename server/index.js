9242ac5902f546f6ea00e9a7fbf29d467fedf63c
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 15:57:40 -0500

    imported routes to index js file

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+import routes from './routes';
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
 
 //Middleware
+app.use('/', routes);
 app.use(cors());
 app.use(express.json());
-
-//Routes
 
 app.listen(8000, () => {
   console.log('server has started on port 8000');
