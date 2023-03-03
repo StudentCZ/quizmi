abe5ea334a6f285a8450ce8787f8079b98004f1a
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 15:56:32 -0500

    created a routes file to hold all the routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes
 
 app.listen(8000, () => {
   console.log('server has started on port 8000');
