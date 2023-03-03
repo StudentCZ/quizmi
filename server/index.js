57e79f6b165700650353dc0a7e5394902f9e7244
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 12:20:36 -0500

    added cors to enable http request

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+//Middleware
 
 app.listen(8000, () => {
   console.log('server has started on port 8000');
