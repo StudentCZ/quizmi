eae989082dcb925ce312725b0582347ad9daa8f7
Chao Zhou <Czhou0k@gmail.com>
2023-03-03 18:52:49 -0500

    got everything working after double checking making sure .env file is connected to pool

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const app = express();
 const cors = require('cors');
 
 //Middleware
-app.use('/', routes);
 app.use(cors());
 app.use(express.json());
+app.use('/', routes);
 
 app.listen(8000, () => {
   console.log('server has started on port 8000');
--- server/routes.js
+++ server/routes.js
@@ -2,4 +2,30 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./db');
 
+router.use(express.json());
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await pool.query(`SELECT * FROM Users`);
+    res.json(users.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+router.post('/users', async (req, res) => {
+  const { username, email, password } = req.body;
+  const query = {
+    text: `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`,
+    values: [username, email, password],
+  };
+  console.log(query); // add this line
+  try {
+    const result = await pool.query(query);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
