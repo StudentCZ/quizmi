cf9f43e55037634aad198116660d74089f7e8938
Chao Zhou <Czhou0k@gmail.com>
2023-03-21 15:27:11 -0400

    modify quizzes table to include references

--- database.sql
+++ database.sql
@@ -10,20 +10,21 @@ CREATE TABLE Categories(
   category_id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
-)
+);
 
 CREATE TABLE Subcategories(
   subcategories_id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   category_id INT NOT NULL REFERENCES Categories(category_id)
-)
+);
 
 CREATE TABLE Quizzes (
   quiz_id SERIAL PRIMARY KEY,
   -- user_id INT NOT NULL REFERENCES Users(user_id),
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  category VARCHAR(255) NOT NULL,
+  category_id INT NOT NULL REFERENCES Categories(category_id),
+  subcategories_id INT REFERENCES Subcategories(subcategories_id),
   -- is_admin_created BOOLEAN NOT NULL DEFAULT true,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
