222d525218ff0a89e8f013f269fe6fc46da54a55
Chao Zhou <Czhou0k@gmail.com>
2023-03-21 12:33:31 -0400

    simplified database schema

--- database.sql
+++ database.sql
@@ -1,18 +1,18 @@
-CREATE TABLE Users (
-  user_id SERIAL PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-);
+-- CREATE TABLE Users (
+--   user_id SERIAL PRIMARY KEY,
+--   username VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL UNIQUE,
+--   password VARCHAR(255) NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
 
 CREATE TABLE Quizzes (
   quiz_id SERIAL PRIMARY KEY,
-  user_id INT NOT NULL REFERENCES Users(user_id),
+  -- user_id INT NOT NULL REFERENCES Users(user_id),
   title VARCHAR(255) NOT NULL,
   description TEXT,
   category VARCHAR(255) NOT NULL,
-  is_admin_created BOOLEAN NOT NULL DEFAULT true,
+  -- is_admin_created BOOLEAN NOT NULL DEFAULT true,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
@@ -41,10 +41,10 @@ CREATE TABLE Scores (
   created_at TIMESTAMP DEFAULT NOW()
 );
 
-CREATE TABLE Leaderboard (
-  leaderboard_id SERIAL PRIMARY KEY,
-  quiz_id INT NOT NULL REFERENCES Quizzes(quiz_id),
-  user_id INT NOT NULL REFERENCES Users(user_id),
-  score INT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-);
+-- CREATE TABLE Leaderboard (
+--   leaderboard_id SERIAL PRIMARY KEY,
+--   quiz_id INT NOT NULL REFERENCES Quizzes(quiz_id),
+--   user_id INT NOT NULL REFERENCES Users(user_id),
+--   score INT NOT NULL,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
