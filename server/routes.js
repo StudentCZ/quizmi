aa3dc62889964da2853eca6dc8afd14b67e6a6a1
Chao Zhou <Czhou0k@gmail.com>
2023-03-21 19:29:00 -0400

    got rid of unused post route

--- server/routes.js
+++ server/routes.js
@@ -4,17 +4,4 @@ const pool = require('./db');
 
 router.use(express.json());
 
-router.post('/users', async (req, res) => {
-  const { username, email, password } = req.body;
-  const queryText = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *`;
-  const queryValues = [username, email, password];
-  try {
-    const { rows } = await pool.query(queryText, queryValues);
-    res.status(201).json(rows[0]);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json(error);
-  }
-});
-
 module.exports = router;
