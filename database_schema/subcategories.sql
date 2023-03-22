c671f4426ab589784cbe4af54dbe596573535288
Chao Zhou <Czhou0k@gmail.com>
2023-03-22 15:25:14 -0400

    added a few more subcategories

--- database_schema/subcategories.sql
+++ database_schema/subcategories.sql
@@ -1,2 +1,2 @@
 INSERT INTO Subcategories(name, category_id)
-VALUES('1st Grade', 1),('1st Grade', 2),('1st Grade', 3),('1st Grade', 4),('1st Grade', 5),('2nd Grade', 1),('2nd Grade', 2),('2nd Grade', 3),('2nd Grade', 4),('2nd Grade', 5),(),(),(),(),(),();
+VALUES('1st Grade', 1),('1st Grade', 2),('1st Grade', 3),('1st Grade', 4),('1st Grade', 5),('2nd Grade', 1),('2nd Grade', 2),('2nd Grade', 3),('2nd Grade', 4),('2nd Grade', 5),('3rd Grade', 1),('3rd Grade', 2),('3rd Grade', 3),('3rd Grade', 4),('3rd Grade', 5),('4th Grade', 1),('4th Grade', 2),('4th Grade', 3),('4th Grade', 4),('4th Grade', 5),;
