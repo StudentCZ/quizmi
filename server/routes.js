34476efcc8ff37854846f75e7299f4dcfec72f65
Chao Zhou <Czhou0k@gmail.com>
2023-03-22 16:01:18 -0400

    added a subcategory get request

--- server/routes.js
+++ server/routes.js
@@ -16,4 +16,14 @@ router.get('/categories', async (req, res) => {
   }
 });
 
+router.get('/subcategories', async (req, res) => {
+  try {
+    const result = await db.query(`SELECT * FROM Subcategories`);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
 module.exports = router;
