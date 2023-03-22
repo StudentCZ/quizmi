ab6542ac139fab4ab22c900411b3880b1f5640ec
Chao Zhou <Czhou0k@gmail.com>
2023-03-22 10:25:50 -0400

    added a get request for categories

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,14 @@ const db = require('./db');
 
 router.use(express.json());
 
-router.get('/categories', async (req, res) => {});
+router.get('/categories', async (req, res) => {
+  try {
+    const result = await db.query(`SELECT * FROM Categories`);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
 
 module.exports = router;
