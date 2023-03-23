bbec53e9edc2c82a6004fae7ac894e8c8719cfb6
Chao Zhou <Czhou0k@gmail.com>
2023-03-22 21:23:23 -0400

    fixed spacing in quizzes sql and created boiler plate for questions

---
+++ database_schema/questions.sql
@@ -0,0 +1,2 @@
+INSERT INTO Questions (quiz_id, subject, question_text, image_url, audio_url )
+VALUES ();
--- database_schema/quizzes.sql
+++ database_schema/quizzes.sql
@@ -1,2 +1,2 @@
 INSERT INTO Quizzes (title, category_id, subcategories_id)
-VALUES ('Math Set 1A', 1, 1 ),('Science 1A', 2,1),('History 1A',3,1),('English',4, 1),('Social Studies', 5, 1);
+VALUES ('Math Set 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English', 4, 1),('Social Studies', 5, 1);
