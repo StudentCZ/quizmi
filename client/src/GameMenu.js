8ed6614599784990cdc0f1dd746e56ca14dd2ab1
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 15:37:14 -0400

    got audio working when loading page

--- client/src/GameMenu.js
+++ client/src/GameMenu.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './GameMenu.module.css';
 import music1 from './audio/Q1.mp3';
 
 const GameMenu = () => {
+  useEffect(() => {
+    const audioElement = document.getElementById('bg-music');
+    document.addEventListener('click', () => {
+      audioElement.play();
+    });
+  }, []);
   return (
     <div className={style.game_menu}>
       <h1 className={style.game_menu_heading}>Welcome To QuizMi</h1>
       <button className={style.game_menu_button}>New Game</button>
       <button className={style.game_menu_button}>Settings</button>
-      <audio id='bg-music' loop controls={false} autoPlay muted>
+      <audio id='bg-music' loop controls={false} autoPlay>
         <source src={music1} type='audio/mp3' />
       </audio>
     </div>
