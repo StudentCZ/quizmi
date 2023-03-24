c4831bf47eaf4c79ed49a633f5bbcb8538296146
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 11:02:33 -0400

    successfully imported gamemenu to app js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import GameMenu from './GameMenu';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <GameMenu />
+    </div>
+  );
 }
 
 export default App;
