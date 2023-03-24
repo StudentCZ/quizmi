1013222b8f068a2ce2f52d16336e0b7f30241360
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 16:38:45 -0400

    wrapped router in app provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './AppContext';
 import GameMenu from './GameMenu';
 import Settings from './Settings';
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<GameMenu />} />
-          <Route path='/settings' element={<Settings />} />
-        </Routes>
-      </Router>
-    </div>
+    <AppProvider>
+      <div className='App'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<GameMenu />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </Router>
+      </div>
+    </AppProvider>
   );
 }
 
