a119980e5d7b8130e84ee621e5ab387ed9575a78
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 13:09:29 -0400

    added class to h1

--- client/src/GameMenu.js
+++ client/src/GameMenu.js
@@ -4,7 +4,7 @@ import style from './GameMenu.module.css';
 const GameMenu = () => {
   return (
     <div className={style.game_menu}>
-      <h1>Welcome To QuizMi</h1>
+      <h1 className={style.game_menu_heading}>Welcome To QuizMi</h1>
       <button className={style.game_menu_button}>New Game</button>
       <button className={style.game_menu_button}>Settings</button>
     </div>
