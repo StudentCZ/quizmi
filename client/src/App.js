6358fddcf255f8803eb8cfa60506db87be09762b
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 16:04:04 -0400

    installed react router and added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GameMenu from './GameMenu';
+import Settings from './Settings';
 
 function App() {
   return (
     <div className='App'>
-      <GameMenu />
+      <Router>
+        <Routes>
+          <Route path='/' element={<GameMenu />} />
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
