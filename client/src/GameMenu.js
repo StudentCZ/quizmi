4a0a61068d27989f52d316811203ef474d4edc15
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 15:26:10 -0400

    renamed audio file nmae

--- client/src/GameMenu.js
+++ client/src/GameMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './GameMenu.module.css';
+import music1 from './audio/Q1.mp3';
 
 const GameMenu = () => {
   return (
@@ -7,6 +8,9 @@ const GameMenu = () => {
       <h1 className={style.game_menu_heading}>Welcome To QuizMi</h1>
       <button className={style.game_menu_button}>New Game</button>
       <button className={style.game_menu_button}>Settings</button>
+      <audio>
+        <source src='' />
+      </audio>
     </div>
   );
 };
