111cc54138586f252b819c67c975bd86a49b0523
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 16:49:34 -0400

    added useContext to game menu

--- client/src/GameMenu.js
+++ client/src/GameMenu.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import style from './GameMenu.module.css';
 import music1 from './audio/Q1.mp3';
 import { Link } from 'react-router-dom';
+import { AppContext } from './AppContext';
 
 const GameMenu = () => {
+  const { musicPlaying, setMusicPlaying } = useContext(AppContext);
   useEffect(() => {
     const audioElement = document.getElementById('bg-music');
     document.addEventListener('click', () => {
+      setMusicPlaying(true);
       audioElement.play();
     });
-  }, []);
+  }, [setMusicPlaying]);
   return (
     <div className={style.game_menu}>
       <h1 className={style.game_menu_heading}>Welcome To QuizMi</h1>
@@ -17,9 +20,11 @@ const GameMenu = () => {
       <Link to='/settings'>
         <button className={style.game_menu_button}>Settings</button>
       </Link>
-      <audio id='bg-music' loop controls={false} autoPlay>
-        <source src={music1} type='audio/mp3' />
-      </audio>
+      {musicPlaying && (
+        <audio id='bg-music' loop controls={false} autoPlay>
+          <source src={music1} type='audio/mp3' />
+        </audio>
+      )}
     </div>
   );
 };
