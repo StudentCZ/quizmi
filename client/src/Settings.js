1e29956f404b8fa615ba7e439e68234b9dca4eff
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 21:56:23 -0400

    added music volume state and function

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from './Settings.module.css';
 
-const Settings = ({ musicPlaying, toggleMusic }) => {
+const Settings = ({
+  musicPlaying,
+  toggleMusic,
+  setMusicVolume,
+  musicVolume,
+}) => {
   const handleToggleMusic = () => {
     toggleMusic();
   };
+  const handleVolumeChange = (event) => {
+    const volume = event.target.value;
+    setMusicVolume(volume);
+  };
+
   return (
     <div>
       <h1>Settings</h1>
@@ -16,6 +26,18 @@ const Settings = ({ musicPlaying, toggleMusic }) => {
         checked={musicPlaying}
         onChange={handleToggleMusic}
       />
+      <div>
+        <label htmlFor='music-volume'>Music Volume</label>
+        <input
+          type='range'
+          min='0'
+          max='1'
+          step='0.1'
+          id='music-volume'
+          value={musicVolume}
+          onChange={handleVolumeChange}
+        />
+      </div>
       <Link to='/'>Back To Main Menu</Link>
     </div>
   );
