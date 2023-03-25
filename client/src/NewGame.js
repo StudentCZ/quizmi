650c9b76b76e7892751aa7f638f8bfed860c5ec7
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 20:58:54 -0400

    added a back to main menu route to newgame

--- client/src/NewGame.js
+++ client/src/NewGame.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NewGame = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <Link to='/'>Back To Main Menu</Link>
+    </div>
+  );
 };
 
 export default NewGame;
