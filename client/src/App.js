6f4d80a16337c37f60f146a1e263a9eb90e6015e
Chao Zhou <Czhou0k@gmail.com>
2023-03-24 20:05:28 -0400

    added music playing state to app js file and passing it down to children files as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './AppContext';
 import GameMenu from './GameMenu';
 import Settings from './Settings';
 
 function App() {
+  const [musicPlaying, setMusicPlaying] = useState(false);
+
+  const toggleMusic = () => {
+    setMusicPlaying(!musicPlaying);
+  };
+
   return (
-    <AppProvider>
-      <div className='App'>
-        <Router>
-          <Routes>
-            <Route path='/' element={<GameMenu />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
-        </Router>
-      </div>
-    </AppProvider>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <GameMenu musicPlaying={musicPlaying} toggleMusic={toggleMusic} />
+            }
+          />
+          <Route
+            path='/settings'
+            element={
+              <Settings musicPlaying={musicPlaying} toggleMusic={toggleMusic} />
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
