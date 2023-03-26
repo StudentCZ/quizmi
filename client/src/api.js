47aff62410a56ab896d6be1f2ef91533ffb4cc7c
Chao Zhou <Czhou0k@gmail.com>
2023-03-26 15:31:17 -0400

    added a boiler plate async function for get request

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
+
 const API_URL = process.env.REACT_APP_API_URL;
+
+const getCategories = async () => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+  }
+};
