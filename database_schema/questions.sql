59733906c190f2c72d16274742341684fe49e420
Chao Zhou <Czhou0k@gmail.com>
2023-03-29 14:44:57 -0400

    change Math to Mathematics to make relationships easier

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -1,5 +1,5 @@
 INSERT INTO Questions (quiz_id, subject, question_text, image_url, audio_url )
-VALUES (1, 'Math', '2+2 = ?', '', ''),
-       (1, 'Math', '1+2 = ?', '', '');
-       (1, 'Math', 'How do you write the number is Two?', '', '');
-       (1, 'Math','Choose a number in the tens place', '', '');
+VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
+       (1, 'Mathematics', '1+2 = ?', '', '');
+       (1, 'Mathematics', 'How do you write the number is Two?', '', '');
+       (1, 'Mathematics','Choose a number in the tens place', '', '');
