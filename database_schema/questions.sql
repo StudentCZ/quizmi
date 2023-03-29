0ac00523aae85e68a29275522488f1177ef95619
Chao Zhou <Czhou0k@gmail.com>
2023-03-29 19:19:58 -0400

    added a few measurements questions

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -3,3 +3,5 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics', '1+2 = ?', '', '');
        (1, 'Mathematics', 'How do you write the number is Two?', '', '');
        (1, 'Mathematics','Choose a number in the tens place', '', '');
+       (1, 'Mathematics', 'What does cm stands for in measurements', '','');
+       (1, 'Mathematics', 'What does mm stands for in measurements', '','');
