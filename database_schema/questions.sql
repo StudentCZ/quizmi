6c449669d91b2e2163e7e11d4e0931496e5a8c99
Chao Zhou <Czhou0k@gmail.com>
2023-03-29 14:10:14 -0400

    added another question for math

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -2,3 +2,4 @@ INSERT INTO Questions (quiz_id, subject, question_text, image_url, audio_url )
 VALUES (1, 'Math', '2+2 = ?', '', ''),
        (1, 'Math', '1+2 = ?', '', '');
        (1, 'Math', 'How do you write the number is Two?', '', '');
+       (1, 'Choose a number in the tens place', '', '');
