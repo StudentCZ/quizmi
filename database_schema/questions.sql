1e401223dd65ce60a28165668017545554d062f1
Chao Zhou <Czhou0k@gmail.com>
2023-03-29 13:47:33 -0400

    added another question

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -1,3 +1,4 @@
 INSERT INTO Questions (quiz_id, subject, question_text, image_url, audio_url )
 VALUES (1, 'Math', '2+2 = ?', '', ''),
        (1, 'Math', '1+2 = ?', '', '');
+       (1, 'Math', 'How do you write the number is Two?', '', '');
