b32d6f62c4ae880ccaa8182c75ab48623e6026e2
Chao Zhou <Czhou0k@gmail.com>
2023-03-30 12:18:41 -0400

    added a questions get request and fix a typo on quizzes error

--- server/routes.js
+++ server/routes.js
@@ -46,7 +46,15 @@ router.get('/quizzes', async (req, res) => {
     res.status(200).json(result.rows);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ error: 'Server Erorr' });
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+router.get('/questions', async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server Error' });
   }
 });
 
