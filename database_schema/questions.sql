161e0ae4c09db6c980e6a83dd2b01f8801d945eb
Chao Zhou <Czhou0k@gmail.com>
2023-03-30 10:19:22 -0400

    fixed capitalization

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -1,7 +1,7 @@
 INSERT INTO Questions (quiz_id, subject, question_text, image_url, audio_url )
 VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics', '1+2 = ?', '', '');
-       (1, 'Mathematics', 'How do you write the number is Two?', '', '');
+       (1, 'Mathematics', 'How do you write the number is two?', '', '');
        (1, 'Mathematics','Choose a number in the tens place', '', '');
        (1, 'Mathematics', 'What does cm stands for in measurements', '','');
        (1, 'Mathematics', 'What does mm stands for in measurements', '','');
