bacbcbf3037dfd56079efdb73fb63d387145724f
Chao Zhou <Czhou0k@gmail.com>
2023-03-30 10:18:34 -0400

    added a few more 1st grade math questions

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -5,3 +5,5 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics','Choose a number in the tens place', '', '');
        (1, 'Mathematics', 'What does cm stands for in measurements', '','');
        (1, 'Mathematics', 'What does mm stands for in measurements', '','');
+       (1, 'Mathematics', 'There are 4 red balls and 2 blue balls. How many balls are there in total?', '', '');
+       (1, 'Mathematics', 'You have 2 pencils and your friend gives you 4 more pencils. How many pencils do you have now?', '', '');
