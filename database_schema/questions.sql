5d11808778e2e8c8f173cc94ff05c0db0b806b42
Chao Zhou <Czhou0k@gmail.com>
2023-03-30 10:49:23 -0400

    added another math question

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -7,3 +7,4 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics', 'What does mm stands for in measurements', '','');
        (1, 'Mathematics', 'There are 4 red balls and 2 blue balls. How many balls are there in total?', '', '');
        (1, 'Mathematics', 'You have 2 pencils and your friend gives you 4 more pencils. How many pencils do you have now?', '', '');
+       (1, 'Mathematics', 'There are 6 children in a class. If 2 more children join the class, how many children are in the class now?', '', '');
