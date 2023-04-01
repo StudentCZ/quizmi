32f2c2c52e7b24872b51972b18f8e30ef8a00541
Chao Zhou <Czhou0k@gmail.com>
2023-03-31 22:19:11 -0400

    added a useEffect to grab data from subcategories table

--- client/src/SubCategories.js
+++ client/src/SubCategories.js
@@ -6,6 +6,15 @@ import style from './SubCategories.module.css';
 const SubCategories = () => {
   const { categoryId } = useParams();
   const [subCategories, setSubCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const subCategoriesData = await getSubCategories(categoryId);
+      subCategories(subCategoriesData);
+    };
+    fetchData();
+  }, [categoryId]);
+
   return <>Hello</>;
 };
 
