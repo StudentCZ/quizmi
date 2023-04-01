4f85c29b9657c827afe0fced04e048d6491fa07e
Chao Zhou <Czhou0k@gmail.com>
2023-03-31 22:35:54 -0400

    got a working subcategory useEffect and data displaying

--- client/src/SubCategories.js
+++ client/src/SubCategories.js
@@ -7,15 +7,30 @@ const SubCategories = () => {
   const { categoryId } = useParams();
   const [subCategories, setSubCategories] = useState([]);
 
+  console.log(subCategories);
+
   useEffect(() => {
     const fetchData = async () => {
       const subCategoriesData = await getSubCategories(categoryId);
-      subCategories(subCategoriesData);
+      setSubCategories(subCategoriesData);
     };
     fetchData();
   }, [categoryId]);
 
-  return <>Hello</>;
+  return (
+    <div>
+      <h1>Choose Sub-Category</h1>
+      <ul>
+        {subCategories.map((subCategory) => {
+          return (
+            <li>
+              <Link>{subCategory.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default SubCategories;
