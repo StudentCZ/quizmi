0f6685288fd3ee199766c07724c84bdbfcedc472
Chao Zhou <Czhou0k@gmail.com>
2023-04-01 13:15:03 -0400

    added imports and css file to select quiz component

--- client/src/SelectQuiz.js
+++ client/src/SelectQuiz.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getQuizzes } from './api';
+import { Link, useParams } from 'react-router-dom';
+import style from './SelectQuiz.module.css';
 
 const SelectQuiz = () => {
   return <div>Hello</div>;
