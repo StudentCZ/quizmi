3f67b112821baf55f0b6ddeaef6d436bb3ec6914
Chao Zhou <Czhou0k@gmail.com>
2023-03-31 22:09:03 -0400

    added imports for subcategories

--- client/src/SubCategories.js
+++ client/src/SubCategories.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getSubCategories } from './api';
+import { Link, useParams } from 'react-router-dom';
+import style from './SubCategories.module.css';
 
 const SubCategories = () => {
   return <>Hello</>;
