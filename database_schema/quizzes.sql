2274fc3fd0268e2f746328d046067b8c9c4f3a54
Chao Zhou <Czhou0k@gmail.com>
2023-04-01 13:40:04 -0400

    revised data in quizzes.sql file

--- database_schema/quizzes.sql
+++ database_schema/quizzes.sql
@@ -1,2 +1,2 @@
 INSERT INTO Quizzes (title, category_id, subcategories_id)
-VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English 1A', 4, 1),('Social Studies 1A', 5, 1), ('Movies 1A', 6);
+VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English 1A', 4, 1),('Social Studies 1A', 5, 1), ('Movies 1A', 6, null);
