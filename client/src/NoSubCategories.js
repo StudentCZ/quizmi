032c012237ca4f5ad70efb377bc1b1e681dc0521
Chao Zhou <Czhou0k@gmail.com>
2023-04-04 11:20:00 -0400

    added class to all tags and a key to every li

--- client/src/NoSubCategories.js
+++ client/src/NoSubCategories.js
@@ -16,15 +16,19 @@ const NoSubCategories = () => {
   }, [categoryId]);
 
   return (
-    <div>
-      <h1>Choose Quiz</h1>
-      <ul>
+    <div className={style.quiz_menu}>
+      <h1 className={style.quiz_heading}>Choose Quiz</h1>
+      <ul className={style.quiz_unordered_list}>
         {quizzes.map((quiz) => {
-          return <li>{quiz.title}</li>;
+          return (
+            <li className={style.quiz_list_item} key={quiz.quiz_id}>
+              {quiz.title}
+            </li>
+          );
         })}
       </ul>
       <Link to='/game/new'>
-        <button>Back</button>
+        <button className={style.quiz_button}>Back</button>
       </Link>
     </div>
   );
