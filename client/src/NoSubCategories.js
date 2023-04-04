5c45b9ca4ea73dba2e667f9ddc96fd0340339c31
Chao Zhou <Czhou0k@gmail.com>
2023-04-04 10:41:55 -0400

    added a heading and import react router dom to include routing

--- client/src/NoSubCategories.js
+++ client/src/NoSubCategories.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './NoSubCategories.module.css';
 
 const NoSubCategories = () => {
   return (
     <div>
       <h1>Choose Quiz</h1>
       <ul></ul>
-      <button></button>
+      <Link>
+        <button></button>
+      </Link>
     </div>
   );
 };
