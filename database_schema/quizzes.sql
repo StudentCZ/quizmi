e8dd43c55b08740d4ff6bbad7c1f8794e1b2dbf8
Chao Zhou <Czhou0k@gmail.com>
2023-04-05 12:39:58 -0400

    fixed data for quizzes

--- database_schema/quizzes.sql
+++ database_schema/quizzes.sql
@@ -1,2 +1,2 @@
 INSERT INTO Quizzes (title, category_id, subcategories_id)
-VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English 1A', 4, 1),('Social Studies 1A', 5, 1), ('Math 2A', 1, 6), ('Math 3A', 1 ,11), ('Math 4A', 1, 16), ('Math 5A', 1, 21), ('Math 6A', 1, 26), ('Math 7A', 1, 31), ('Math 6A', 1, 36);
+VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 2),('History 1A', 3, 3),('English 1A', 4, 4),('Social Studies 1A', 5, 5), ('Math 2A', 1, 6), ('Math 3A', 1 ,11), ('Math 4A', 1, 16), ('Math 5A', 1, 21), ('Math 6A', 1, 26), ('Math 7A', 1, 31), ('Math 6A', 1, 36), ();
