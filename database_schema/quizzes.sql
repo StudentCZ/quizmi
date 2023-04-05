89657bcf3b9bd49639795a6cf796bc4561fa1499
Chao Zhou <Czhou0k@gmail.com>
2023-04-05 09:04:43 -0400

    added all subcategory math quizzes from grade 1-8

--- database_schema/quizzes.sql
+++ database_schema/quizzes.sql
@@ -1,2 +1,2 @@
 INSERT INTO Quizzes (title, category_id, subcategories_id)
-VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English 1A', 4, 1),('Social Studies 1A', 5, 1), ('Math 2A', 1, 6), ('Movies 1A', 6, null);
+VALUES ('Math 1A', 1, 1 ),('Science 1A', 2, 1),('History 1A', 3, 1),('English 1A', 4, 1),('Social Studies 1A', 5, 1), ('Math 2A', 1, 6), ('Math 3A', 1 ,11), ('Math 4A', 1, 16), ('Math 5A', 1, 21), ('Math 6A', 1, 26), ('Math 7A', 1, 31), ('Math 6A', 1, 36);
