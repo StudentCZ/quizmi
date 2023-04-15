92808ebe17a68db76706d833edc1a068d24c9d87
Chao Zhou <Czhou0k@gmail.com>
2023-04-15 12:03:35 -0400

    fixed fetchData function so it can retrieve the answers data correctly

--- client/src/PlayGame.js
+++ client/src/PlayGame.js
@@ -12,14 +12,15 @@ const PlayGame = () => {
       const questionsData = await getQuizQuestions(quizId);
       const questionAnswers = await Promise.all(
         questionsData.map(async (question) => {
-          const answers = await getQuestionAnswers(question.id);
+          const answers = await getQuestionAnswers(question.question_id);
+          console.log(answers, 'Hello');
           return {
             ...question,
             answers,
           };
         })
       );
-      setQuestions(questionsData);
+      setQuestions(questionAnswers);
     };
     fetchData();
   }, [quizId]);
