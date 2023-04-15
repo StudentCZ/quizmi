7ce3b89a27619b98fdacbe3d2e7528ff4782e88a
Chao Zhou <Czhou0k@gmail.com>
2023-04-15 11:51:32 -0400

    combine answers to questions in fetchdata function

--- client/src/PlayGame.js
+++ client/src/PlayGame.js
@@ -10,6 +10,15 @@ const PlayGame = () => {
   useEffect(() => {
     const fetchData = async () => {
       const questionsData = await getQuizQuestions(quizId);
+      const questionAnswers = await Promise.all(
+        questionsData.map(async (question) => {
+          const answers = await getQuestionAnswers(question.id);
+          return {
+            ...question,
+            answers,
+          };
+        })
+      );
       setQuestions(questionsData);
     };
     fetchData();
