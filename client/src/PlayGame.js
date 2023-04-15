8a32f5d6fe0f2d94513e16806881fc4dfdec0c90
Chao Zhou <Czhou0k@gmail.com>
2023-04-14 20:09:34 -0400

    added more imports

--- client/src/PlayGame.js
+++ client/src/PlayGame.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { getQuizQuestions } from './api';
+import style from './PlayGame.module.css';
 
 const PlayGame = () => {
   return <div>Hello</div>;
