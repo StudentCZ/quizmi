3eddbc58d1114b4f9edecfbb5a084a4e6576a0da
Chao Zhou <Czhou0k@gmail.com>
2023-04-17 11:43:56 -0400

    added more questions

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -8,4 +8,5 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics', 'There are 4 red balls and 2 blue balls. How many balls are there in total?', '', ''),
        (1, 'Mathematics', 'You have 2 pencils and your friend gives you 4 more pencils. How many pencils do you have now?', '', ''),
        (1, 'Mathematics', 'There are 6 children in a class. If 2 more children join the class, how many children are in the class now?', '', '');
-       (1, 'Mathematics', 'Which is bigger 1 cm or 1 inch', '', '');
+       (1, 'Mathematics', 'Which is bigger 1 cm or 1 inch?', '', '');
+       (1, 'Mathematics', 'How much is a penny worth?', '', '')
