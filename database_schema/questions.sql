238dcf5326afff7836154442d4c7cabccc759b16
Chao Zhou <Czhou0k@gmail.com>
2023-04-17 11:44:26 -0400

    fixed syntax

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -9,4 +9,4 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (1, 'Mathematics', 'You have 2 pencils and your friend gives you 4 more pencils. How many pencils do you have now?', '', ''),
        (1, 'Mathematics', 'There are 6 children in a class. If 2 more children join the class, how many children are in the class now?', '', '');
        (1, 'Mathematics', 'Which is bigger 1 cm or 1 inch?', '', '');
-       (1, 'Mathematics', 'How much is a penny worth?', '', '')
+       (1, 'Mathematics', 'How much is a penny worth?', '', '');
