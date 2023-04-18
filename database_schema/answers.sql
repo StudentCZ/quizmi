2b9faca2f75a04c18026c6491e0842b696b15b58
Chao Zhou <Czhou0k@gmail.com>
2023-04-18 12:05:40 -0400

    added another answer to question for testing

--- database_schema/answers.sql
+++ database_schema/answers.sql
@@ -1,1 +1,1 @@
-INSERT INTO Answers(question_id, answer_text, is_correct) VALUES (1, '4', true), (1, '5', false), (1 , '1', false), (1, '2', false);
+INSERT INTO Answers(question_id, answer_text, is_correct) VALUES (1, '4', true), (1, '5', false), (1 , '1', false), (1, '2', false),(1, '7', false),(1, '4', false),(1, '5', false), (1, '3', true);
