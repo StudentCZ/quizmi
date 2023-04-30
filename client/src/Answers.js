8877f2840ce704fec487c69a02e11b125cc22db6
Chao Zhou <Czhou0k@gmail.com>
2023-04-30 09:27:19 -0400

    passed all props to answers

--- client/src/Answers.js
+++ client/src/Answers.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import style from './Answers.module.css';
+
+const Answers = ({
+  answerId,
+  answerText,
+  isSelected,
+  handleAnswerSelect,
+  disabled,
+}) => {};
