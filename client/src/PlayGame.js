bf588fb1c2b14b4b5eb893870b295123091201bc
Chao Zhou <Czhou0k@gmail.com>
2023-04-30 10:39:31 -0400

    added selectedanswer state to help keep track of score

--- client/src/PlayGame.js
+++ client/src/PlayGame.js
@@ -42,6 +42,7 @@ const PlayGame = () => {
       selectedAnswer: newSelectedAnswer,
     };
     setQuestions(updatedQuestions);
+    setSelectedAnswer(newSelectedAnswer);
     setShowNextButton(true);
   };
 
