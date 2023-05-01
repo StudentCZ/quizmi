002e545cc7b94df897c232d3a56883cfcbd02011
Chao Zhou <Czhou0k@gmail.com>
2023-05-01 10:48:59 -0400

    added h1 tag to score

--- client/src/FinalScore.js
+++ client/src/FinalScore.js
@@ -5,8 +5,12 @@ import style from './FinalScore.module.css';
 const FinalScore = () => {
   const location = useLocation();
   const score = location.state?.score ?? 0;
-  console.log(score);
-  return <div className={style.score_menu}>Your score is: {score}</div>;
+
+  return (
+    <div className={style.score_menu}>
+      <h1>Your score is: {score}</h1>
+    </div>
+  );
 };
 
 export default FinalScore;
