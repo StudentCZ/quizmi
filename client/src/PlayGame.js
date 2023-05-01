3eceb542ed3235689e0e8e181edb8ea0b300306b
Chao Zhou <Czhou0k@gmail.com>
2023-05-01 10:17:16 -0400

    updated the useeffect to include local storage

--- client/src/PlayGame.js
+++ client/src/PlayGame.js
@@ -22,21 +22,25 @@ const PlayGame = () => {
     const savedQuizProgress = JSON.parse(
       localStorage.getItem(`quiz-${quizId}-progress`)
     );
-    const fetchData = async () => {
-      const questionsData = await getQuizQuestions(quizId);
-      const questionAnswers = await Promise.all(
-        questionsData.map(async (question) => {
-          const answers = await getQuestionAnswers(question.question_id);
-          return {
-            ...question,
-            answers,
-            selectedAnswer: null,
-          };
-        })
-      );
-      setQuestions(questionAnswers);
-    };
-    fetchData();
+    if (savedQuizProgress) {
+      setQuizProgress(savedQuizProgress);
+    } else {
+      const fetchData = async () => {
+        const questionsData = await getQuizQuestions(quizId);
+        const questionAnswers = await Promise.all(
+          questionsData.map(async (question) => {
+            const answers = await getQuestionAnswers(question.question_id);
+            return {
+              ...question,
+              answers,
+              selectedAnswer: null,
+            };
+          })
+        );
+        setQuestions(questionAnswers);
+      };
+      fetchData();
+    }
   }, [quizId]);
 
   const currentQuestion = questions[currentQuestionIndex];
