1fb466f2a7b122492486603e66db10f39371edf2
Chao Zhou <Czhou0k@gmail.com>
2023-05-01 13:43:48 -0400

    added a continue button

--- client/src/GameMenu.js
+++ client/src/GameMenu.js
@@ -32,7 +32,7 @@ const GameMenu = ({ musicPlaying, toggleMusic }) => {
       <Link to='/game/new'>
         <button className={style.game_menu_button}>New Game</button>
       </Link>
-
+      <button className={style.game_menu_button}>Continue Game</button>
       <Link to='/settings'>
         <button className={style.game_menu_button}>Settings</button>
       </Link>
