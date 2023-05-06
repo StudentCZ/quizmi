593789d243f941db910127e29ed89ebb6683c672
Chao Zhou <Czhou0k@gmail.com>
2023-05-06 11:30:55 -0400

    added path file to server for hosting into production

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
+const path = require('path');
 
 //Middleware
 app.use(cors());
