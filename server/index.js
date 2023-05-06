e6d0a8d9553665cb32b8a96ecad01a2ed10abe0b
Chao Zhou <Czhou0k@gmail.com>
2023-05-06 13:53:59 -0400

    changing apporach on how to host app to aws

--- server/index.js
+++ server/index.js
@@ -2,23 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
-const path = require('path');
-
-const _dirname = path.dirname('');
-const buildPath = path.join(_dirname, '../client/build');
-
-app.use(express.static(buildPath));
-
-app.get('/*', function (req, res) {
-  res.sendFile(
-    path.join(__dirname, '../client/build/index.html'),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
 
 //Middleware
 app.use(cors());
