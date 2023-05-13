99bb7eb155088acb92fb329a217a56705cda7adf
Chao Zhou <Czhou0k@gmail.com>
2023-05-13 12:52:33 -0400

    added another history grade 1 question

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -26,5 +26,6 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (2, 'Science','What type of bird eats animals that are already dead?', '',''),
        (3, 'History', 'How many states are there in the U.S.?', '', ''),
        (3, 'History', 'Which U.S. city is known as “the windy city”?', '', ''),
-       (3, 'History', 'Which is the smallest U.S. state?')
+       (3, 'History', 'Which is the smallest U.S. state?'),
+       (3, 'History', 'Which U.S. state is known as the Sunflower State?', '', '')
        ;
