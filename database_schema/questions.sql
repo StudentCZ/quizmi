0e885a2ada575ee58093ed68e129abb0d266cf63
Chao Zhou <Czhou0k@gmail.com>
2023-05-17 16:53:07 -0400

    added another history question to db

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -32,5 +32,6 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (3, 'History', 'What is Las Vegas famous for?', '', ''),
        (3, 'History', 'In which state would you find the Grand Canyon?', '', ''),
        (3, 'History', 'Who was the first president of the United State?', '', ''),
-       (3, 'History', 'What year was the Declaration Of Independence signed?', '', '')
+       (3, 'History', 'What year was the Declaration Of Independence signed?', '', ''),
+       (3, 'History', 'What is the name of the U.S national anthem?', '', '')
        ;
