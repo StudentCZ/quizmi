22dc89806fee2ca651b1262a19391f19207d17fb
Chao Zhou <Czhou0k@gmail.com>
2023-05-18 11:12:01 -0400

    added test code for game menu component

--- client/src/GameMenu.test.js
+++ client/src/GameMenu.test.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import GameMenu from './GameMenu';
+
+test('renders GameMenu component', () => {
+  render(<GameMenu />);
+
+  // eslint-disable-next-line no-restricted-globals
+  const headingElement = screen.getByText('QuizMi');
+  expect(headingElement).toBeInTheDocument();
+});
