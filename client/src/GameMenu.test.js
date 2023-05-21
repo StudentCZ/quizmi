20587e9a63250c6ec1ae7447cb571cb2d748c08c
Chao Zhou <Czhou0k@gmail.com>
2023-05-21 11:17:37 -0400

    imported router to test file and wrap gamemenu inside of router

--- client/src/GameMenu.test.js
+++ client/src/GameMenu.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import GameMenu from './GameMenu';
 
 test('renders GameMenu component', () => {
-  render(<GameMenu />);
+  render(
+    <Router>
+      <GameMenu />
+    </Router>
+  );
 
   // eslint-disable-next-line no-restricted-globals
   const headingElement = screen.getByText('QuizMi');
