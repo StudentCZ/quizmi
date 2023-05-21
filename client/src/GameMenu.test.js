4a881cb3e7a324b7139cfa96fe783f20ef0b9cea
Chao Zhou <Czhou0k@gmail.com>
2023-05-21 11:30:17 -0400

    imported testing jest dom to test file

--- client/src/GameMenu.test.js
+++ client/src/GameMenu.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GameMenu from './GameMenu';
 
