a73a3a222880d1c65b877257df2243fa11ae5a17
Chao Zhou <Czhou0k@gmail.com>
2023-05-22 03:28:00 -0400

    added test to game menu to test new game button

--- client/src/GameMenu.test.js
+++ client/src/GameMenu.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GameMenu from './GameMenu';
 
-test('renders GameMenu component', () => {
+test('renders GameMenu component QuizMi', () => {
   render(
     <Router>
       <GameMenu />
@@ -13,4 +13,15 @@ test('renders GameMenu component', () => {
 
   const headingElement = screen.getByText('QuizMi');
   expect(headingElement).toBeInTheDocument();
+
+  test('renders New Game Button', () => {
+    render(
+      <Router>
+        <GameMenu />
+      </Router>
+    );
+
+    const newGameButton = screen.getByText('New Game');
+    expect(newGameButton).toBeInTheDocument();
+  });
 });
