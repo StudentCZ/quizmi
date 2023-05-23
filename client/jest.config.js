2544aee509412bd504951b84bf2c67b93023fdf8
Chao Zhou <Czhou0k@gmail.com>
2023-05-23 08:59:49 -0400

    added module to make testing with css possible in jest config file

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
