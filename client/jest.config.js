c3ead4e2c003262a6d5dae1d0be51508a7e80738
Chao Zhou <Czhou0k@gmail.com>
2023-05-23 08:51:14 -0400

    updated jest config file to handle axios

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
   },
+  preset: 'ts-jest',
   transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
 };
