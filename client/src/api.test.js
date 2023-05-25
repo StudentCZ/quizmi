65504dc62f23367078ddca4b67bcbb94b4ac9198
Chao Zhou <Czhou0k@gmail.com>
2023-05-25 09:42:07 -0400

    imported all the restful api function to file for testing

---
+++ client/src/api.test.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import {
+  getCategories,
+  getSubCategories,
+  getSubCategoryQuiz,
+  getNoSubCategoryQuiz,
+  getQuizQuestions,
+  getQuestionAnswers,
+} from './api';
