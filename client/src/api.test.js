c8b57701dd4765fbb2b1d31bc7618a526112a3d2
Chao Zhou <Czhou0k@gmail.com>
2023-05-25 09:52:52 -0400

    added a test to mock api category function

--- client/src/api.test.js
+++ client/src/api.test.js
@@ -13,3 +13,26 @@ jest.mock('axios');
 afterEach(() => {
   jest.resetAllMocks();
 });
+
+describe('API functions', () => {
+  it('gets categories', async () => {
+    const categories = [
+      {
+        category_id: 1,
+        name: 'Mathematics',
+      },
+      {
+        category_id: 2,
+        name: 'Science',
+      },
+    ];
+    axios.get.mockResolvedValue({ data: categories });
+
+    const result = await getCategories();
+
+    expect(result).toEqual(categories);
+    expect(axios.get).toHaveBeenCalledWith(
+      `${process.env.REACT_APP_API_URL}/categories`
+    );
+  });
+});
