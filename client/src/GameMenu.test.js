aaabdc43d4eb73826a9bdaf8f51a5574ca600f1a
Chao Zhou <Czhou0k@gmail.com>
2023-05-28 10:23:21 -0400

    added imports of newgame and settings components for testing routes

--- client/src/GameMenu.test.js
+++ client/src/GameMenu.test.js
@@ -2,13 +2,10 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import {
-  BrowserRouter as Router,
-  MemoryRouter,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import GameMenu from './GameMenu';
+import NewGame from './NewGame';
+import Settings from './Settings';
 
 test('renders GameMenu component QuizMi', () => {
   render(
