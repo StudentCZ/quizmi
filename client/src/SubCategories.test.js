9c021ebbcfba49bd44ad91ba3a6239849d2b1a3d
Chao Zhou <Czhou0k@gmail.com>
2023-05-30 08:58:17 -0400

    changing import to memory router for better testing

--- client/src/SubCategories.test.js
+++ client/src/SubCategories.test.js
@@ -1,7 +1,9 @@
+/* eslint-disable testing-library/prefer-find-by */
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import SubCategories from './SubCategories';
 import { getSubCategories } from './api';
+
+jest.mock('./api');
