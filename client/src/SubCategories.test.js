966624035db8281471635764072c603bec241fec
Chao Zhou <Czhou0k@gmail.com>
2023-05-30 09:26:30 -0400

    added a jest mock function call to mock test subcategory api function

--- client/src/SubCategories.test.js
+++ client/src/SubCategories.test.js
@@ -2,8 +2,31 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import SubCategories from './SubCategories';
 import { getSubCategories } from './api';
 
-jest.mock('./api');
+jest.mock('./api', () => ({
+  getSubCategories: jest.fn(),
+}));
+
+test('displays a list of subcategories', async () => {
+  const mockSubCategories = [
+    { subcategories_id: '1', name: 'Subcategory 1' },
+    { subcategories_id: '2', name: 'Subcategory 2' },
+  ];
+
+  getSubCategories.mockResolvedValueOnce(mockSubCategories);
+
+  render(
+    <MemoryRouter initialEntries={['/category/1']}>
+      <Routes>
+        <Route path='category/:categoryId' element={<SubCategories />} />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  const subCategoryItems = await waitFor(() => screen.getAllByRole('listitem'));
+
+  expect(subCategoryItems).toHaveLength(mockSubCategories.length);
+});
