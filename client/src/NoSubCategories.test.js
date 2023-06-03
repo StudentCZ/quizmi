990603cc25050c2b19a1862c82aa663162d1ce42
Chao Zhou <Czhou0k@gmail.com>
2023-06-03 10:46:11 -0400

    got a working test to fetch data for a category without subcategories

--- client/src/NoSubCategories.test.js
+++ client/src/NoSubCategories.test.js
@@ -4,3 +4,31 @@ import '@testing-library/jest-dom';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import NoSubCategories from './NoSubCategories';
 import { getNoSubCategoryQuiz } from './api';
+
+jest.mock('./api', () => ({
+  getNoSubCategoryQuiz: jest.fn(),
+}));
+
+test('fetches for a category without subcategories', async () => {
+  const mockQuizzes = [
+    { quiz_id: '15', title: 'Trivia 1A' },
+    { quiz_id: '16', title: 'Trivia 1B' },
+  ];
+
+  getNoSubCategoryQuiz.mockResolvedValueOnce(mockQuizzes);
+
+  render(
+    <MemoryRouter initialEntries={['/category/1/quizzes']}>
+      <Routes>
+        <Route
+          path='/category/:categoryId/quizzes'
+          element={<NoSubCategories />}
+        />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  const quizItems = await waitFor(() => screen.findAllByRole('listitem'));
+
+  expect(quizItems).toHaveLength(mockQuizzes.length);
+});
