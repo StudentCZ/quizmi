1a8e0cee734e5f5e9f61274ed5c626b091381a45
Chao Zhou <Czhou0k@gmail.com>
2023-06-03 11:16:02 -0400

    removed errorMessage state to handle errors in a different way

--- client/src/NoSubCategories.js
+++ client/src/NoSubCategories.js
@@ -7,23 +7,20 @@ import style from './NoSubCategories.module.css';
 const NoSubCategories = () => {
   const { categoryId } = useParams();
   const [quizzes, setQuizzes] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const quizData = await getNoSubCategoryQuiz(categoryId);
         setQuizzes(quizData);
-        setErrorMessage('');
-      } catch (error) {
-        setErrorMessage('Failed to load quizzes');
-      }
+      } catch (error) {}
     };
     fetchData();
   }, [categoryId]);
 
   return (
     <div className={style.quiz_menu}>
+      {errorMessage && <p>{errorMessage}</p>}
       <h1 className={style.quiz_heading}>Choose Quiz</h1>
       {quizzes.length > 0 ? (
         <ul className={style.quiz_unordered_list}>
