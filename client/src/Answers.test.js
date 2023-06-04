1b72dc20a9e6941de8b1d22e0535b10a68a092ad
Chao Zhou <Czhou0k@gmail.com>
2023-06-04 09:55:30 -0400

    added a test to answers to check if component is rendering

--- client/src/Answers.test.js
+++ client/src/Answers.test.js
@@ -2,3 +2,17 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Answers from './Answers';
+
+test('component is rendering correctly', () => {
+  render(
+    <Answers
+      answerId='1'
+      answerText='9'
+      isSelected={false}
+      handleAnswerSelect={() => {}}
+      disabled={false}
+    />
+  );
+
+  expect(screen.getByText('9')).toBeInTheDocument();
+});
