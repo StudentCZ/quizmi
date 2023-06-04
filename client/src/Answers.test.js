98659b694b5beaded0ca1520f0ad3389e1b49301
Chao Zhou <Czhou0k@gmail.com>
2023-06-04 10:13:02 -0400

    added a mock test to test handleAnswerSelect is functioning correctly

--- client/src/Answers.test.js
+++ client/src/Answers.test.js
@@ -33,3 +33,20 @@ test('applies selected answer class when isSelected is true', () => {
     'selected_answer'
   );
 });
+
+test('calls handleAnswerSelect with answerId when clicked', () => {
+  const mockHandleAnswerSelect = jest.fn();
+
+  render(
+    <Answers
+      answerId='1'
+      answerText='9'
+      isSelected={false}
+      handleAnswerSelect={mockHandleAnswerSelect}
+      disabled={false}
+    />
+  );
+
+  fireEvent.click(screen.getByText('9'));
+  expect(mockHandleAnswerSelect).toHaveBeenCalledWith('1');
+});
