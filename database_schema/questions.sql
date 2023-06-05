a381713d4a9e862295bd8a93e14d7faab8d43d28
Chao Zhou <Czhou0k@gmail.com>
2023-06-05 11:26:49 -0400

    added another History question

--- database_schema/questions.sql
+++ database_schema/questions.sql
@@ -37,5 +37,7 @@ VALUES (1, 'Mathematics', '2+2 = ?', '', ''),
        (3, 'History', 'What year was the Declaration Of Independence signed?', '', ''),
        (3, 'History', 'What is the name of the U.S national anthem?', '', ''),
        (3, 'History', 'How old do you have to be to vote?', '', ''),
-       (3, 'History', 'What ship did the pilgrims sail on to America', '', '')
+       (3, 'History', 'What ship did the pilgrims sail on to America', '', ''),
+       (3, 'History', 'Who said this famous quote "The British are coming!"', '', '')
+
        ;
