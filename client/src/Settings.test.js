f9cfb5dc1103e7b67acad69d89806551cd435d58
Chao Zhou <Czhou0k@gmail.com>
2023-06-06 01:16:22 -0400

    added test for settings component to see if component renders properly

--- client/src/Settings.test.js
+++ client/src/Settings.test.js
@@ -1,4 +1,26 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom';
 import Settings from './Settings';
+
+test('component is rendering correctly', () => {
+  render(
+    <Router>
+      <Settings
+        musicPlaying={false}
+        toggleMusic={() => {}}
+        setMusicVolume={() => {}}
+        musicVolume={0.5}
+        theme='orange'
+        handleThemeChange={() => {}}
+      />
+    </Router>
+  );
+
+  expect(screen.getByText('Settings')).toBeInTheDocument();
+  expect(screen.getByText('Select Theme')).toBeInTheDocument();
+  expect(screen.getByText('Enable/Disable Music')).toBeInTheDocument();
+  expect(screen.getByText('Music Volume')).toBeInTheDocument();
+  expect(screen.getByText('Main Menu')).toBeInTheDocument();
+});
